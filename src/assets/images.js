1752d64af8de0e71b71a0e3ec51250daa3ec2afd
rtk-group <mydevclass9690@gmail.com>
2026-09-06 21:35:03 +0530

    fix deployment issue

--- src/assets/images.js
+++ src/assets/images.js
@@ -5,9 +5,6 @@ import img5 from "./img5.jpg"
 import img6 from "./img6.jpg"
 import img7 from "./img7.jpg"
 import img8 from "./img8.jpg"
-import scaleImg from "./scaleImg.jpg"
-import firstflore from "./firstflore.jpg"
-import ground from "./ground.jpg"
 import home_hero from "./home_stayimg.png"
 
 export const images = {
